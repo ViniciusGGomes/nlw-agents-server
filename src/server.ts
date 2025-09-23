a2ad5ad42633115c62b9cbe40b47e757f8b5c8ff
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-09-23 22:28:34 +0000

    feat(server): set up Fastify server with CORS, Zod validation and health check

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import { fastify } from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { fastifyCors } from "@fastify/cors";
+import { env } from "./env.ts";
+import { sql } from "../db/connection.ts";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "http://localhost:5137",
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get("/health", () => {
+  return "ok";
+});
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log("Server is running");
+});
