bcfb9f2e2109d06b60bccef624ab183c4ece490d
Vinícius Gomes <161797273+ViniciusGGomes@users.noreply.github.com>
2025-09-26 22:32:09 +0000

    remove

--- db/connection.ts
+++
@@ -1,9 +0,0 @@
-import postgres from "postgres";
-import { env } from "../src/env.ts";
-
-// Vai permitir eu fazer as queries para o meu db
-export const sql = postgres(env.DATABASE_URL);
-
-const result = await sql`SELECT 'Hello' as message`;
-
-console.log(result);
